19d103eeec4be409ac5555e4bdecd1cfd83af276
hauntedghoul <120139937+hauntedghoul@users.noreply.github.com>
2023-08-18 09:18:35 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,46 @@
 console.log("JS Loaded!!!");
 
 const express = require('express');
-const session = require('express-session');
-
-const pug = require('pug');
 const bcrypt = require('bcrypt');
+const { dal } = require('./dal/dal');
 
 const app = express();
 
-const port = 1000;
+let port = 2000;
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+// app.use(express.urlencoded());
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
 
 app.get("/login", (req, res) => {
-
     res.render("login");
-})
+});
 
-//app.post("/login")
-
-app.get("/login", (res, res) => {
-
-})
-
-app.get("/signup", (req, res) => {
+app.get("/register", (req, res) => {
     res.render("register");
+});
+
+app.post("/register", async (req, res) =>{
+    const {username, password, confirmPassword, email, confrimEmail} = req.body;
+    
+    await dal.createUser(username, password, confirmPassword, email, confrimEmail);
+    res.redirect("/login");
 })
 
-
-app.get("/profile", (req,res) => {
-    const usuario = req.session.usuario;
-    res.render("profile", {usuario})
-})
-
+app.get("/profile", (req, res) => {
+    res.render("profile");
+});
 
 app.listen(port, () => {
-    console.log("Charlie is listening to your " + port);
+    console.log("express listening on port", port);
 });
