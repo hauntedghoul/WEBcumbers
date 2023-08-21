fd48ad4ced3352806c3a817331c4aae9b6409660
ChelseaBui11 <141456665+ChelseaBui11@users.noreply.github.com>
2023-08-21 14:57:01 -0600

    Current code

--- app.js
+++ app.js
@@ -6,17 +6,14 @@ const { dal } = require('./dal/dal');
 
 const app = express();
 
-let port = 2000;
+let port = 2001;
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.use(express.static('public'));
-
-app.use(express.json());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-// app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -30,15 +27,25 @@ app.get("/register", (req, res) => {
     res.render("register");
 });
 
-app.post("/register", async (req, res) =>{
-    const {username, password, confirmPassword, email, confrimEmail} = req.body;
-    
-    await dal.createUser(username, password, confirmPassword, email, confrimEmail);
-    res.redirect("/login");
-})
-
 app.get("/profile", (req, res) => {
     res.render("profile");
+});
+
+// Handle the user registration form submission
+app.post('/register', async (req, res) => {
+    const { username, password, passwordConfirm, email, emailConfirm } = req.body;
+
+    try {
+        // Additional validation and checks can be added here before calling createUser
+
+        const savedUser = await dal.createUser(username, password, passwordConfirm, email, emailConfirm);
+        
+        // Handle success scenario, e.g., redirect to a success page
+        res.redirect('/profile'); // Assuming you want to redirect to the profile page
+    } catch (error) {
+        // Handle error scenario, e.g., redirect to an error page
+        res.redirect('/register'); // Redirect back to registration page on error
+    }
 });
 
 app.listen(port, () => {
--- dal.js
+++ dal.js
@@ -1,73 +1,42 @@
-const { mongoose, Schema } = require('mongoose');
-const bcrypt = require('bcrypt');
-const { urlencoded } = require('express');
-
-const connectionString = "mongodb://127.0.0.1:27017/User";
-
-mongoose.connect(connectionString, {useUnifiedTopology: true, useNewParser: true});
+const {mongoose, Schema} = require('mongoose');
+// const bcrypt = require('bcrypt');
+const connectionString = "mongodb://localhost:27017/Userio";
+//mongodb+srv://Johanna:306879@usuario.uejhtxs.mongodb.net/
+mongoose.connect(connectionString, { useUnifiedTopology: true, useNewUrlParser: true });
 const connection = mongoose.connection;
 
 connection.once("open", () => {
-    console.log("Mongoose Successfully connected");
-});
+    console.log("goose Successfully connected");
+})
 
-const Usuario = new Schema(
+const UserSchema = new mongoose.Schema(
     {
         username: String,
         password: String,
-        confirmPassword:String ,
+        passwordConfirm: String,
         email: String,
-        confirmEmail: String
-
+        emailConfirm: String
     },
-    {collection: "info"}
+    { collection: "Users" }
 );
 
-
-const infoCollection = mongoose.model("info", Usuario);
-
+const UserModel = mongoose.model('User', UserSchema);
 
 exports.dal = {
-    getallData: async function() {
-        const data = await infoCollection.find({}).lean();
-        
-        return data
-    },
-
-    get: async function() {
-        let result = await infoCollection.find({}).lean();
-
-        console.log(result);
-    },
-
-    createUsuario: async function (username, password, confirmPassword, email, confirmEmail) {
-        const newUsuario = new infoCollection({
-            username: username,
-            password: password,
-            confirmPassword: confirmPassword,
-            email: email,
-            confirmEmail: confirmEmail
-        });
-
-        const userSaved = await newUsuario.save();
-        console.log("New user was added: ", userSaved);
-        return userSaved;
-    },
-
-
-    getUsuario: async function (username) {
+    createUser: async function (username, password, passwordConfirm, email, emailConfirm) {
         try {
-            const usuario = await infoCollection.findOne({username: username}).exec();
-            return usuario
-        }catch (error){
-            console.log("error retrieving usuario", error);
-            return null;
+            const newUser = new UserModel({
+                username,
+                password,
+                passwordConfirm,
+                email,
+                emailConfirm
+            });
+
+            const savedUser = await newUser.save();
+            return savedUser;
+        } catch (error) {
+            throw error;
         }
-    },
-
-
-
-    
+    }
 };
-
-
